924212850cb6d48692d6f89b48a204ed4a9b3035
Dzmitry Lisouski <dmtrqerlly@gmail.com>
2020-03-16 17:51:33 +0100

    Add SelectionSort

--- SortingNumbers/src/Main.java
+++ SortingNumbers/src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
 
+    private static void selectionSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int min = array[i];
+            int minId = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minId = j;
+                }
+            }
+            int buf = array[i];
+            array[i] = min;
+            array[minId] = buf;
+        }
+    }
+
     private static void bubbleSort(int[] array) {
         boolean flag = false;
         int buf;
@@ -20,6 +36,13 @@ public class Main {
         int[] bubbleArray = new int[]{6, 4, 2, 7, 124, 64, 125, 1, 2, 35, 2, 32};
         bubbleSort(bubbleArray);
         for (int value : bubbleArray) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+
+        int[] selectionArray = new int[]{2, 4, 1, 6, 7, 1, 5, 42, 25, 124, 64};
+        selectionSort(selectionArray);
+        for (int value : selectionArray) {
             System.out.print(value + " ");
         }
         System.out.println();
