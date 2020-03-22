dececf6294a7d461aa7702b28aa8523590aae8a4
Sebastian <spieta2@gmail.com>
2020-03-22 22:16:46 +0100

    quick sort with random pivot done

--- Lab1/sortingmethods.py
+++ Lab1/sortingmethods.py
@@ -9,19 +9,17 @@ def insert_sort(array):
             j-=1
         array[j] = value
 
-def quick_sort(array):
-    quick_sort_rec(array, 0, len(array)-1)
-
 def partition(array, startIndex, endIndex):
     pivotIndex = randint(startIndex, endIndex)
-    pivot = array[pivotIndex]
-    smallerIndex = startIndex-1
-    for currentIndex in range(startIndex, endIndex):
+    array[startIndex], array[pivotIndex] = array[pivotIndex], array[startIndex]
+    pivot = array[startIndex]
+    smallerIndex = startIndex
+    for currentIndex in range(startIndex+1, endIndex+1):
         if array[currentIndex] < pivot:
             smallerIndex+=1
-            array[smallerIndex], array[currentIndex] = array[currentIndex], array[smallerIndex]
-    array[smallerIndex+1], array[pivotIndex] = array[pivotIndex], array[smallerIndex+1]
-    return (smallerIndex+1)
+            array[currentIndex], array[smallerIndex] = array[smallerIndex], array[currentIndex]
+    array[smallerIndex], array[startIndex] = array[startIndex], array[smallerIndex]
+    return smallerIndex
 
 def quick_sort_rec(array, startIndex, endIndex):
     if startIndex < endIndex: 
@@ -29,7 +27,9 @@ def quick_sort_rec(array, startIndex, endIndex):
         quick_sort_rec(array, startIndex, partitionIndex-1)
         quick_sort_rec(array, partitionIndex+1, endIndex)
 
+def quick_sort(array):
+    quick_sort_rec(array, 0, len(array)-1)
 
-numbers = [234, 4, -2, 15, 9, -30, 999]
+numbers = [4, 8, 9, 3, -59, 123, -345, 10, -10, 10]
 quick_sort(numbers)
 print(numbers)
