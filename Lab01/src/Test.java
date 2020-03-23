0db1dbdca763fec764d56e480e19869ea22be1c7
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-03-23 15:02:53 +0100

    update Test.java

--- Lab01/src/Test.java
+++ Lab01/src/Test.java
@@ -1,7 +1,16 @@
 public class Test {
 
     public static void main(String[] args) {
+        int[] arr0 = { 15, 9, 24, 2, 7, 71, 68, 13, 10, 52, 12, 14, 23, 85, 41 };
+        int[] arr1 = { 15, 9, 24, 2, 7, 71, 68, 13, 10, 52, 12, 14, 23, 85, 41 };
 
+        BubbleSort.sort(arr0);
+        InsertSort.sort(arr1);
+
+        System.out.println("bubble sort:");
+        printArray(arr0);
+        System.out.println("insert sort:");
+        printArray(arr1);
     }
 
     private static void printArray(int[] arr) {
