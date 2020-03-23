2d1b827e615096dd126160fb65081185dfeccdfe
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-03-23 14:06:52 +0100

    update BubbleSort.java

--- Lab01/src/BubbleSort.java
+++ Lab01/src/BubbleSort.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class BubbleSort {
+
+    static void bubbleSort (int[] arr) {
+        int n = arr.length;
+        int tmp;
+        for (int i = 1; i < n; ++i)
+            for (int j = 1; j < n - i; ++j)
+                if (arr[j - 1] > arr[j]) {
+                    tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+    }
+
 }
