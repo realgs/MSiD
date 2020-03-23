59a0d4b4731fc89466a99b1fb8319bd2224eec23
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-03-23 14:54:28 +0100

    update Test.java, create method printArray

--- Lab01/src/Test.java
+++ Lab01/src/Test.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Test {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
 }
