40cb247d12e901a7ad53cc7c30b04fb3a4f5539b
MichalEgier <egierm@wp.pl>
2020-03-23 15:01:12 -0700

    added Selectsort

--- SortingAlgorythms.py
+++ SortingAlgorythms.py
@@ -1,5 +1,3 @@
-import array
-
 def replace(arr,i,j):
 	elem = arr[i]
 	arr[i] = arr[j]
@@ -16,8 +14,26 @@ def bubbleSort(arr):
 			if arr[j+1] < arr[j]: 			
 				replace(arr,j+1,j)	
 		n = n+1
-			
+
+def selectSort(arr):
+	
+	for i in range(0,len(arr)-1):
+		indexOfMin = i
+		minElem = arr[i]
+		for j in range(i+1,len(arr)-1):
+			if arr[j] < minElem:
+				minElem = arr[j]
+				indexOfMin = j		
+		replace(arr,i,indexOfMin)
+
 arr = [1,7,2,4,10,13]
+
+for i in arr:
+	print(i)
+
+print('\n')
+
+selectSort(arr)
 
 for i in arr:
 	print(i)
