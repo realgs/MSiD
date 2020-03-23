a33bd90a4635d35f1a87ec3a0b0186aebf335a91
Piasta <katarzyna.piasta@intel.com>
2020-03-23 20:30:28 +0100

    fixes + tests

--- SortingAlgoritms/src/Main.java
+++ SortingAlgoritms/src/Main.java
@@ -1,34 +1,33 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String args[]){
-        List<Integer> list = new ArrayList<>();
+        List<Integer> list_integers = new ArrayList<>();
         Integer[] tab = {123, 12, 122, 34, 34, -12, 4, 127, 42, 8, 3};
-        for (int i = 0; i < tab.length; i++) {
-            list.add(tab[i]);
+        Collections.addAll(list_integers,tab);
 
-        }
-        QuickSort.sort(list, Integer::compareTo);
-        System.out.println(list.toString());
-
-        List<Double> list1 = new ArrayList<>();
+        List<Double> list_doubles = new ArrayList<>();
         Double[] tab2 = {1.0,3.5, 12.1, 3.4,1.1,32.1};
-        for (int i = 0; i < tab2.length; i++) {
-            list1.add(tab2[i]);
+        Collections.addAll(list_doubles,tab2);
 
-        }
-        QuickSort.sort(list1, Double::compareTo);
-        System.out.println(list1.toString());
+        List<String> list_strings = new ArrayList<>();
+        String[] strings = {"kot","pies","zając", "krowa", "biedronka", "moj_brat_michal"};
+        Collections.addAll(list_strings, strings);
+
 
         List<Character> characters = new ArrayList<Character>();
         characters.add('a');
         characters.add('x');
         characters.add('z');
-       characters.add('q');
+        characters.add('q');
 
-        QuickSort.sort(characters, Character::compareTo);
-        System.out.println(characters.toString());
+
+        System.out.println(MergeSort.sortIntegers(list_integers).toString());
+        System.out.println(QuickSort.sortDoubles(list_doubles).toString());
+        System.out.println(MergeSort.sortStrings(list_strings).toString());
+        System.out.println(QuickSort.sortCharacters(characters).toString());
 
 
 
--- SortingAlgoritms/src/MergeSort.java
+++ SortingAlgoritms/src/MergeSort.java
@@ -1,14 +1,49 @@
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class MergeSort extends Sort {
 
+    public static List<String> sortStrings(List<String> list){
+        return sort(list, String::compareTo);
 
-    public static <T> List<T> split(List<T> inputArray, Comparator<? super T> comparator) {
+    }
+
+    public static List<Character> sortCharacters(List<Character> list){
+        return sort(list, Character::compareTo);
+
+    }
+
+    public static List<Double> sortDoubles(List<Double> list){
+        return sort(list, Double::compareTo);
+
+    }
+
+    public static List<Integer> sortIntegers(List<Integer> list){
+        return sort(list, Integer::compareTo);
+
+    }
+
+    public static <T> List<T> sort(List<T> list, Comparator<? super T> comparator) {
+
+
+        if (list.size() > 1) {
+            split(list,comparator);
+
+        }
+        LinkedList<T> result = new LinkedList<T>(list);
+
+
+        return result;
+
+    }
+
+
+    private static <T> List<T> split(List<T> inputArray, Comparator<? super T> comparator) {
         if (inputArray.size() > 1) {
             List<T> left = new ArrayList<T>();
-            List<T> right= new ArrayList<T>();
+            List<T> right = new ArrayList<T>();
             for (int i = 0; i < inputArray.size(); i++) {
                 if (i < inputArray.size() / 2) {
                     left.add(inputArray.get(i));
@@ -16,15 +51,16 @@ public class MergeSort extends Sort {
                     right.add(inputArray.get(i));
                 }
             }
-            split(left,comparator);
-            split(right,comparator);
-            quick(inputArray, left, right,comparator);
+            split(left, comparator);
+            split(right, comparator);
+            merge(inputArray, left, right, comparator);
         }
 
         return inputArray;
 
     }
-    public static <T> void quick(List<T> result, List<T> left, List<T> right,Comparator<? super T> comparator) {
+
+    private static <T> void merge(List<T> result, List<T> left, List<T> right, Comparator<? super T> comparator) {
         int i1 = 0;
         int i2 = 0;
 
