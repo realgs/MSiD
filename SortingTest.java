0852c30031b5a6fe761f0bd5504bdd3118a789c6
Konrad <konrad.hajduga@gmail.com>
2020-03-23 01:10:35 +0100

    Shell sort and testing class improvement

--- SortingTest.java
+++ SortingTest.java
@@ -6,16 +6,22 @@ public class SortingTest {
 
     public void runTest() {
         BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+        printResult(bubbleSort);
 
-        if (checkCorrectness(bubbleSort)) {
-            System.out.println("Sorting algorithm is working properly");
+        ShellSort<Integer> shellSort = new ShellSort<>();
+        printResult(shellSort);
+    }
+
+    private void printResult(ArrayListSorter<Integer> sorter){
+        if (checkCorrectness(sorter)) {
+            System.out.println(sorter.getClass().getName() + " algorithm is working properly");
         } else {
-            System.out.println("Sorting algorithm isn't working properly");
+            System.out.println(sorter.getClass().getName() + " algorithm isn't working properly");
         }
     }
 
     private boolean checkCorrectness(ArrayListSorter<Integer> sorter) {
-        ArrayList<Integer> testList = createArrayList(7000);
+        ArrayList<Integer> testList = createArrayList(10000);
         ArrayList<Integer> sortingAlgorithmList = new ArrayList<>(testList);
 
         Collections.sort(testList);
