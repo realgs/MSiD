697e4954b32371a11ae1b9fc8d89499355cfe616
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-03-23 15:19:00 +0100

    update Test.java

--- Lab01/src/Test.java
+++ Lab01/src/Test.java
@@ -1,8 +1,11 @@
 public class Test {
 
     public static void main(String[] args) {
-        int[] arr0 = { 15, 9, 24, 2, 7, 71, 68, 13, 10, 52, 12, 14, 23, 85, 41 };
-        int[] arr1 = { 15, 9, 24, 2, 7, 71, 68, 13, 10, 52, 12, 14, 23, 85, 41 };
+        int[] arr0 = { 15, 9, 24, 2, 2, 7, 71, 68, 13, 10, 52, 71, 12, 14, 23, 85, 41 };
+        int[] arr1 = { 15, 9, 24, 2, 2, 7, 71, 68, 13, 10, 52, 71, 12, 14, 23, 85, 41 };
+
+        int[] arr2 = { 5, 4, 3, 2, 1};
+        int[] arr3 = { 5, 4, 3, 2, 1};
 
         BubbleSort.sort(arr0);
         InsertSort.sort(arr1);
@@ -11,6 +14,24 @@ public class Test {
         printArray(arr0);
         System.out.println("insert sort:");
         printArray(arr1);
+        System.out.println();
+
+        BubbleSort.sort(arr2);
+        InsertSort.sort(arr3);
+
+        System.out.println("bubble sort:");
+        printArray(arr2);
+        System.out.println("insert sort:");
+        printArray(arr3);
+        System.out.println();
+
+        BubbleSort.sort(arr2);
+        InsertSort.sort(arr3);
+
+        System.out.println("bubble sort:");
+        printArray(arr2);
+        System.out.println("insert sort:");
+        printArray(arr3);
     }
 
     private static void printArray(int[] arr) {
