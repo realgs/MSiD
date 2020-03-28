0c554bcddc27ac0849f3e31f21d332f71fe00a8e
wang <rav5738@gmail.com>
2020-03-28 01:40:55 +0100

    implement random array generator

--- SortingAlgorithms/src/Main.java
+++ SortingAlgorithms/src/Main.java
@@ -1,19 +1,37 @@
+import java.util.Random;
+
 public class Main {
+
+  public static double[] generateArray(int size){
+    double[] retArr = new double[size];
+    Random rand = new Random();
+
+    for(int i = 0; i < size; i++){
+      retArr[i] = (double) Math.round(size * 100 * rand.nextDouble()) / 100;
+    }
+
+    return retArr;
+
+  }
 
   public static void main(String[] args) {
 
     final double[] unsortedList = {1.6, -4, 2.2, 5, 2.2};
+    final double[] generatedList = generateArray(10);
 
     SortingAlgorithm heapSort = new HeapSort();
     SortingAlgorithm quickSort = new QuickSort();
     SortingAlgorithm insertionSort = new InsertionSort();
+    SortingAlgorithm pancakeSort = new PancakeSort();
 
-    heapSort.sort(unsortedList, 5);
+    heapSort.sort(generatedList, 5);
     System.out.println(heapSort.toString());
-    quickSort.sort(unsortedList, 5);
+    quickSort.sort(generatedList, 5);
     System.out.println(quickSort.toString());
-    insertionSort.sort(unsortedList, 5);
+    insertionSort.sort(generatedList, 5);
     System.out.println(insertionSort.toString());
+    pancakeSort.sort(generatedList, 5);
+    System.out.println(pancakeSort.toString());
 
   }
 
