1b3a600bc401f4d08c22b997b0b4235e225d8304
Wi1ku <wilczek20@gmail.com>
2020-03-29 22:51:24 +0200

    cleanups

--- SortingMethods.py
+++ SortingMethods.py
@@ -12,21 +12,21 @@ class SortingMethods:
                 if sortable_list[j] > sortable_list[j + 1]:
                     self.swap(sortable_list, j, j + 1)
 
-    def partition(self, array, low, high):
-        pivot = array[high]
-        i = low
-        for j in range(low, high):
+    def partition(self, array, start_index, end_index):
+        pivot = array[end_index]
+        i = start_index
+        for j in range(start_index, end_index):
             if array[j] < pivot:
                 self.swap(array, i, j)
                 i = i + 1
-        self.swap(array, i, high)
+        self.swap(array, i, end_index)
         return i
 
-    def quick_sort_ranged(self, array, low, high):
-        if low < high:
-            p = self.partition(array, low, high)
-            self.quick_sort_ranged(array, low, p - 1)
-            self.quick_sort_ranged(array, p + 1, high)
+    def quick_sort_ranged(self, array, start_index, end_index):
+        if start_index < end_index:
+            p = self.partition(array, start_index, end_index)
+            self.quick_sort_ranged(array, start_index, p - 1)
+            self.quick_sort_ranged(array, p + 1, end_index)
 
     def quick_sort(self, array):
         self.quick_sort_ranged(array, 0, len(array)-1)
@@ -35,5 +35,4 @@ class SortingMethods:
 arr = [1, 999.99, 420.69, -22, -1.1]
 sm = SortingMethods()
 sm.quick_sort(arr)
-for i in range(len(arr)):
-    print("%.2f" % arr[i]),
+print(arr)
