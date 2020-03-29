132f4a1d5d8b1c59e29f520dd015818605788a20
Igor Walkowiak <IgorWalkowiak@protonmail.com>
2020-03-29 16:57:18 +0200

    Added SelectSort method

--- presentation.py
+++ presentation.py
@@ -1,17 +1,26 @@
 import random
 from bubbleSort import bubbleSort
 from insertSort import insertSort
+from selectSort import selectSort
 
-testTab = [random.randint(-100,100) for i in range(15)]
+
+testTab = [random.randint(-100,100) for i in range(5)]
 print("Before bubble sort sort:")
 print(testTab)
 bubbleSort(testTab)
 print("After bubble sort sort:")
 print(testTab)
 
-testTab = [random.randint(-100,100) for i in range(15)]
+testTab = [random.randint(-100,100) for i in range(5)]
 print("Before insert sort sort:")
 print(testTab)
 insertSort(testTab)
 print("After insert sort sort:")
+print(testTab)
+
+testTab = [random.randint(-100,100) for i in range(5)]
+print("Before select sort sort:")
+print(testTab)
+selectSort(testTab)
+print("After select sort sort:")
 print(testTab)
