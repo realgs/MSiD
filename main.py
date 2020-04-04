70d2473803a7d0fa84f4ed5fa7b53610a2daf525
alexHackerNoob <47832121+alexHackerNoob@users.noreply.github.com>
2020-04-04 17:33:47 +0200

    add function that count difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import time
 
 criptoCurrencies =[
     "BTC", #Bitcoin
@@ -27,16 +28,31 @@ def getCurrentAskBid():
     to_ret = ""
     counter = 0
     for currency in criptoCurrencies:
-        arrAskBid = getStepString(currency)
+        arrAskBid = getArrBidAsk(currency)
         to_ret += criptoCurrenciesNames[counter]  + " bid: " + str(arrAskBid[0]) + " ask: " + str(arrAskBid[1]) +" in USD"+ "\n"
         counter+=1
     print(to_ret)
-def getStepString(currency):
+def getArrBidAsk(currency):
     url = "https://bitbay.net/API/Public/" + currency + "USD/" + "ticker.json"
     json_obj = requests.get(url)
     json_obj_text = json_obj.text
     parsed_string = json.loads(json_obj_text)
     return [parsed_string["bid"], parsed_string["ask"]]
 
-
-getCurrentAskBid()
+def diffenceBidAsk():
+    while True:
+        to_ret = ""
+        count = 0
+        for currency in criptoCurrencies:
+            arrBidAsk = getArrBidAsk(currency)
+            proc =  (arrBidAsk[1] - arrBidAsk[0])/arrBidAsk[0]
+            proc = round(proc, 2)
+            #to_ret+=criptoCurrenciesNames[count] + ": " + round(proc, 2) + "%\n"
+            t = time.localtime()
+            current_time = time.strftime("%H:%M:%S", t)
+            to_ret += criptoCurrenciesNames[count]+ ": " + "difference in procent: " + str(proc) +"% current time: "+ current_time + "\n"
+            count+=1
+        print(to_ret)
+        time.sleep(5)
+#getCurrentAskBid()
+diffenceBidAsk()
