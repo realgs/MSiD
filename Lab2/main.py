3fbd5575b83e5c7d3057e794680002c94a5a27eb
Kraft_ <kraftmajnmail@gmail.com>
2020-04-05 17:26:35 +0200

    comparator added

--- Lab2/main.py
+++ Lab2/main.py
@@ -1,30 +1,14 @@
 from sortingmethods import quick_sort, insert_sort, bubble_sort, merge_sort
+from sortingcomparator import compareAll
 from random import randint
 
 def main():
     minValue = -100
     maxValue = 100
-    arraySize = 100
-    array1 = [randint(minValue, maxValue) for _ in range(0, arraySize)]
-    array2 = array1.copy()
-    array3 = array1.copy()
-    array4 = array1.copy()
-
-    print("Before insertion sort: \n%s" % array1)
-    insert_sort(array1)
-    print("After inserion sort: \n%s" % array1)
-
-    print("Before quick sort: \n%s" % array2)
-    quick_sort(array2)
-    print("After quick sort: \n%s" % array2)
-
-    print("Before bubble sort: \n%s" % array3)
-    bubble_sort(array3)
-    print("After bubble sort: \n%s" % array3)
-
-    print("Before merge sort: \n%s" % array4)
-    merge_sort(array4)
-    print("After merge sort: \n%s" % array4)
+    arraySize = 5000
+    array = [randint(minValue, maxValue) for _ in range(0, arraySize)]
+    functions = [bubble_sort, insert_sort, quick_sort, merge_sort]
+    compareAll(functions, array)
 
 if __name__ == "__main__":
     main()
