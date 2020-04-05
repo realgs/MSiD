905342c78a385a667c6d3d95fa2304478101c5d7
Elunniva <elunniva@wp.pl>
2020-04-05 17:51:21 +0200

    added shell sort

--- SortingMethods/src/Main.java
+++ SortingMethods/src/Main.java
@@ -9,6 +9,7 @@ public class Main {
     String arr3[]={"ala","kuba","ania","adam","patryk"};
     Integer arr2[] = {2,52,12,71,3,85,23};
     Double arr4[] = {12.32, 3.4, 3.5, 8.56, 1.24};
+    Double arr5[] = {12.32, 3.4, 3.5, 8.56, 1.24};
 
     SortingAlgorithm<Integer> quickSort= new QuickSort();
     quickSort.sort(arr1);
@@ -20,11 +21,14 @@ public class Main {
     mergeSort.sort(arr2);
     SortingAlgorithm<Double> bitonicSort = new BitonicSort();
     bitonicSort.sort(arr4);
+    SortingAlgorithm<Double> shellSort = new ShellSort();
+    shellSort.sort(arr5);
     System.out.println(Arrays.toString(arr));
     System.out.println(Arrays.toString(arr1));
     System.out.println(Arrays.toString(arr3));
     System.out.println(Arrays.toString(arr2));
     System.out.println(Arrays.toString(arr4));
+    System.out.println(Arrays.toString(arr5));
 
     }
 }
