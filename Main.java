b45def803e35fc38ae9acf990a34e64e2d073339
Vesker7 <jskonieczny07@gmail.com>
2020-04-06 20:58:11 +0200

    Added HeapSort and QuickSort

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.util.Random;
 
 public class Main 
 {
+	final int size = 40000;
+	int [] los_liczby = random_array(size);
+	
 	private static int [] random_array(int n)
 	{
 		int [] liczby = new int[n];
@@ -16,18 +19,18 @@ public class Main
 	
 	public static long comparesort(Sorting sorting_method)
 	{
-		final int size = 40000;
-		int [] los_liczby = random_array(size);
-		
 		return sorting_method.sort(los_liczby);
 	}	
 	
 	public static void main(String[] args) 
 	{
-		Sorting bubble = new BubbleSort(), select = new SelectSort();
+		Sorting bubble = new BubbleSort(), select = new SelectSort(),
+				heap = new HeapSort(), quick = new QuickSort();
 		
 		System.out.println("Bubble Sort: " + comparesort(bubble));
 		System.out.println("Select Sort: " + comparesort(select));
+		System.out.println("Heap Sort: " + comparesort(heap));
+		System.out.println("Quick Sort: " + comparesort(quick));
 		System.out.println("____________________________________");
 	}
 
