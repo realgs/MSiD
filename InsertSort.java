b417893504a0c2dc02ca131b6af40a7ba16cd944
Radosław <shakiman666@gmail.com>
2020-04-08 23:24:57 +0200

    Add files from l1

---
+++ InsertSort.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class InsertSort<T extends Comparable<? super T>> implements ListSorter<T> {
+    @Override
+    public void sort(ArrayList<T> list) {
+        for (int i = 1; i < list.size(); ++i) {
+            T value = list.get(i);
+            int j;
+            for (j = i; j > 0 && value.compareTo(list.get(j-1))<0; j--)
+                list.set(j,list.get(j-1));
+            list.set(j, value);
+        }
+    }
+}
