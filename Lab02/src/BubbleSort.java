98793ce9cff3b6b2e9e6dbc01810612f2279ef87
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-04-10 15:28:36 +0200

    add previous files

---
+++ Lab02/src/BubbleSort.java
@@ -0,0 +1,15 @@
+public class BubbleSort {
+
+    static void sort (int[] arr) {
+        int n = arr.length;
+        int tmp;
+        for (int i = 1; i < n; ++i)
+            for (int j = 1; j <= n - i; ++j)
+                if (arr[j - 1] > arr[j]) {
+                    tmp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = tmp;
+                }
+    }
+
+}
