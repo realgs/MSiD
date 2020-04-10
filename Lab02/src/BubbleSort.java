abde838c87b3bbb474be3f019a41f8150546cb51
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-04-10 17:22:08 +0200

    update Comparison.java

--- Lab02/src/BubbleSort.java
+++ Lab02/src/BubbleSort.java
@@ -9,7 +9,6 @@ public class BubbleSort<T extends Comparable<? super T>> implements ArrayListSor
         for (int i = 1; i < size; ++i)
             for (int j = 1; j <= size - i; ++j)
                 if (list.get(j - 1).compareTo(list.get(j)) > 0) {
-                    //arr[j - 1] > arr[j]
                     tmp = list.get(j - 1);
                     list.set(j - 1, list.get(j));
                     list.set(j, tmp);
--- Lab02/src/Comparison.java
+++ Lab02/src/Comparison.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
-public class Comparison {
+public class Comparison<T> {
+
+    public long executionTime(ArrayListSorting<T> alg, ArrayList<T> list) {
+        ArrayList<T> tmp = new ArrayList<>(list);
+        long start = System.currentTimeMillis();
+        alg.sort(tmp);
+        return System.currentTimeMillis() - start;
+    }
 }
