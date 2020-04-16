a5da4b658e2e3b8d3b6c3eb172bfbf0235e89121
oazaboy <22818302+oazaboy@users.noreply.github.com>
2020-04-16 18:47:24 +0200

    update printing bids and asks

--- Lab03/bitbaymarket.py
+++ Lab03/bitbaymarket.py
@@ -6,14 +6,23 @@ asks = []
 
 
 def print_orderbook(crypto, currency):
+    get_orderbook(crypto, currency)
+    print('BIDS')
+    for bid in bids:
+        print(f'\t{bid[1]} (rate: {bid[0]})')
+    print('-' * 30)
+    print('ASKS')
+    for ask in asks:
+        print(f'\t{ask[1]} (rate: {ask[0]})')
+
+
+def orderbook_url(crypto, currency):
+    return f'https://bitbay.net/API/Public/{crypto}{currency}/orderbook.json'
+
+
+def get_orderbook(crypto, currency):
     global bids
     global asks
     orderbook = requests.get(orderbook_url(crypto, currency)).json()
     bids = orderbook['bids']
     asks = orderbook['asks']
-    print(bids)
-    print(asks)
-
-
-def orderbook_url(crypto, currency):
-    return f'https://bitbay.net/API/Public/{crypto}{currency}/orderbook.json'
