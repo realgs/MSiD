2f52a622f81b0341bc8acebcbaaf6cf4c290915e
Reevo <radoslawkarbowiak@gmail.com>
2020-04-20 00:46:14 +0200

    Lista 2 added

---
+++ L2/mergeSort.py
@@ -0,0 +1,37 @@
+def mergeSort(array):
+    if len(array) > 1:
+        middle = len(array) // 2
+  
+        leftArr = array[:middle]
+        rightArr = array[middle:]
+
+        mergeSort(leftArr) 
+        mergeSort(rightArr)
+
+        merge(array, leftArr, rightArr)
+
+def merge(array, leftArr, rightArr):
+    i = 0
+    j = 0
+
+    k = 0
+
+    while i < len(leftArr) and j < len(rightArr):
+        if leftArr[i] < rightArr[j]:
+            array[k] = leftArr[i]
+            i += 1
+        else:
+            array[k] = rightArr[j]
+            j += 1
+
+        k += 1
+    
+    while i < len(leftArr):
+        array[k] = leftArr[i]
+        i += 1
+        k += 1
+
+    while j < len(rightArr):
+        array[k] = rightArr[j]
+        j += 1
+        k += 1
