424a4330bcaf1a22f8b98711c9b2c33dd91df80a
TheManis92 <mateuszkogut007@gmail.com>
2020-04-23 21:40:03 +0100

    Final changes

--- dataFromApi.py
+++ dataFromApi.py
@@ -13,19 +13,20 @@ def printOfferList(offer_type,list_title, list):
     for elem in list:
         print(offer_type + " Rate: " + str(elem[0]) + " Amount of Cryptocurrency: " + str(elem[1]))
 
+def getDiffPercentage(buy_list, sell_list):
+    best_buy_offer = buy_list[0][0]
+    best_sell_offer = sell_list[0][0]
+    print("BUY/SELL PERCENT DIFFERENCE " + str((best_sell_offer - best_buy_offer) / (best_buy_offer) * 100))
 
 def main():
-    threading.Timer(5.0, main).start()
     url = "https://bitbay.net/API/Public/BTC/orderbook.json"
     parsed = parsedData(url)
     buy_list = parsed["bids"]
     sell_list = parsed["asks"]
     printOfferList("Buy","BUY OFFERS",buy_list)
     printOfferList("Sell", "SELL OFFERS",sell_list)
-    best_buy_offer = buy_list[0][0]
-    best_sell_offer = sell_list[0][0]
-    print("BUY/SELL PERCENT DIFFERENCE " + str((best_sell_offer - best_buy_offer) / (best_buy_offer) * 100))
- 
+    getDiffPercentage()
+    
 def runMainEvery5Seconds():
     try:
         threading.Timer(5.0, runMainEvery5Seconds).start()
