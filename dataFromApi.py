3a20be79b00470988a33bdb21b77516bcfccbaf0
TheManis92 <mateuszkogut007@gmail.com>
2020-04-23 21:30:33 +0100

    Add repeat every 5 seconds

--- dataFromApi.py
+++ dataFromApi.py
@@ -8,23 +8,31 @@ def parsedData(url):
     parsed = json.loads(data)
     return parsed
 
-def printOfferList(list):
+def printOfferList(offer_type,list_title, list):
+    print(list_title)
     for elem in list:
-        print("Rate: " + str(elem[0]) + " Amount of Cryptocurrency: " + str(elem[1]))
+        print(offer_type + " Rate: " + str(elem[0]) + " Amount of Cryptocurrency: " + str(elem[1]))
 
 
 def main():
+    threading.Timer(5.0, main).start()
     url = "https://bitbay.net/API/Public/BTC/orderbook.json"
     parsed = parsedData(url)
     buy_list = parsed["bids"]
     sell_list = parsed["asks"]
-    print("BUY OFFERS")
-    printOfferList(buy_list)
-    print("SELL OFFERS")
-    printOfferList(sell_list)
+    printOfferList("Buy","BUY OFFERS",buy_list)
+    printOfferList("Sell", "SELL OFFERS",sell_list)
     best_buy_offer = buy_list[0][0]
     best_sell_offer = sell_list[0][0]
-    print((best_sell_offer - best_buy_offer) / (best_buy_offer) * 100)
+    print("BUY/SELL PERCENT DIFFERENCE " + str((best_sell_offer - best_buy_offer) / (best_buy_offer) * 100))
+ 
+def runMainEvery5Seconds():
+    try:
+        threading.Timer(5.0, runMainEvery5Seconds).start()
+        main()
+        print("CTRL+C TO STOP")
+    except KeyboardInterrupt:
+        pass
 
 if __name__ == "__main__":
-    main()
+    runMainEvery5Seconds()
