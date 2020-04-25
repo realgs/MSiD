3f97fb154b8ae688fb3c952d7952b6da19163451
Kamil <kogol99@gmail.com>
2020-04-25 02:02:04 +0200

    Add auto_transaction_history

--- Lab3.py
+++ Lab3.py
@@ -1,3 +1,4 @@
+import time
 import requests
 
 
@@ -7,7 +8,6 @@ def search_market(market_currency, base_currency):
     markets = requests.get("https://api.bittrex.com/api/v1.1/public/getmarkets")
     markets_json = markets.json()
     if markets_json["success"]:
-        print("Udalo się pobrać \"markets\" !")
         markets_records = markets_json["result"]
         for record in markets_records:
             if (record["MarketCurrency"] == market_currency or ["MarketCurrencyLong"] == market_currency) and (
@@ -19,24 +19,55 @@ def search_market(market_currency, base_currency):
 
 def transaction_history(market_currency, base_currency, amount_orders):
     market = search_market(market_currency, base_currency)
-    name_of_market_currency = market["MarketCurrency"]
-    name_of_base_currency = market["BaseCurrency"]
     if market is not None:
+        name_of_market_currency = market["MarketCurrency"]
+        name_of_base_currency = market["BaseCurrency"]
         last_transaction = requests.get("https://api.bittrex.com/api/v1.1/public/getorderbook?market=" +
                                         name_of_base_currency + "-" + name_of_market_currency + "&type=both")
         last_transaction_json = last_transaction.json()
         print("Ostatnie oferty kupna " + name_of_base_currency + "-" + name_of_market_currency + ":")
         for i in range(amount_orders):
-            print("\t" + str(last_transaction_json["result"]["buy"][i]["Rate"]))
+            print("\t" + str(last_transaction_json["result"]["buy"][i]["Rate"]) + " (qty: " +
+                  str(last_transaction_json["result"]["buy"][i]["Quantity"]) + ")")
+            if i == 99:
+                break
         print("Ostatnie oferty sprzedaży " + name_of_base_currency + "-" + name_of_market_currency + ":")
         for i in range(amount_orders):
-            print("\t" + str(last_transaction_json["result"]["sell"][i]["Rate"]))
+            print("\t" + str(last_transaction_json["result"]["sell"][i]["Rate"]) + " (qty: " +
+                  str(last_transaction_json["result"]["sell"][i]["Quantity"]) + ")")
+            if i == 99:
+                break
     else:
         print("Podana kombinacja walut nie istnieje na ryneczku :(")
 
 
+def auto_transaction_history(market_currency, base_currency, amount_orders, time_period):
+    print("\nTrwa uruchamianie automatycznego wypisu danych z rynku \n")
+    market = search_market(market_currency, base_currency)
+    if market is not None:
+        name_of_market_currency = market["MarketCurrency"]
+        name_of_base_currency = market["BaseCurrency"]
+    else:
+        print("Podana kombinacja walut nie istnieje na ryneczku :(")
+    while market is not None:
+        last_transaction = requests.get("https://api.bittrex.com/api/v1.1/public/getorderbook?market=" +
+                                        name_of_base_currency + "-" + name_of_market_currency + "&type=both")
+        last_transaction_json = last_transaction.json()
+        transaction_history(market_currency, base_currency, amount_orders)
+        print("Różnica między kupnem, a sprzedażą " + name_of_base_currency + "-" + name_of_market_currency + ":")
+        for i in range(amount_orders):
+            print("\t" + str((1 - (last_transaction_json["result"]["sell"][i]["Rate"] -
+                                   last_transaction_json["result"]["buy"][i]["Rate"]) /
+                             last_transaction_json["result"]["sell"][i]["Rate"])) + "%")
+            if i == 99:
+                break
+        print("\n")
+        time.sleep(time_period)
+
+
 def main():
-    transaction_history("ETH", "USD", 5)
+    transaction_history("ETH", "BTC", 15)
+    auto_transaction_history("ETH", "USD", 10, 5)
 
 
 if __name__ == '__main__':
