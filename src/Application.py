12939b7b9a748857267390bf7b221ade2bd3ea46
= <38375497+amsok@users.noreply.github.com>
2020-05-02 23:27:56 +0200

    Assigments 1-3. Comparing prizes of cryptocurrencies between diffrent markets

--- src/Application.py
+++ src/Application.py
@@ -1,23 +1,93 @@
 import requests
-import datetime
-import time
 
-CURRENCIES = ['BTC','BCC','LTC', 'ETH']
-AMOUNTOFOFFERS = 3
-def printOffers(amount, currency, bid, ask):
-    print(currency +" buy/sell = {}%".format(str(round((1-(ask[0][0]-bid[0][0])/ask[0][0]),4))))
-    print("bids offers:\tasks offers")
-    print("{0}:USD\t\t\t{0}:USD".format(currency))
-    for i in range(amount):
-        print(str(bid[i][0])+"\t\t\t"+str(ask[i][0]))
-    print()
+CURRENCIES = ['ETH','LTC','TRX','XRP']
+FEE = {}
+FEE['tokok'] = 0.002
+FEE['bitbay'] = 0.001
+FEE['latoken'] = 0.001
+FEE['biki'] = 0.0015
+FEE['hitbtc'] = 0.002
 
-while True:
-    x = datetime.datetime.now()
-    print(x)
-    for currency in CURRENCIES:
-        requestLink = 'https://bitbay.net/API/Public/{}/orderbook.json'.format(currency)
-        respons = requests.get(requestLink)
-        printOffers(AMOUNTOFOFFERS,currency,respons.json()["bids"],respons.json()["asks"])
-    print("\n")
-    time.sleep(10)
+def getBTCtoPLN():
+    r = requests.get("https://bitbay.net/API/Public/BTCPLN/orderbook.json")
+    return float(r.json()['asks'][0][0])
+
+
+
+wallet =1000
+outcome = wallet / getBTCtoPLN()
+
+# api data for diffrent markets - since many sites require sign in I've found that in someone else's repo
+# tokok = "https://www.tokok.com/api/v1/depth?symbol={}_BTC"
+# bitbay = "https://bitbay.net/API/Public/{}BTC/orderbook.json"
+# biki = "https://openapi.biki.com/open/api/market_dept?symbol={}btc&type=step0"
+# latoken = "https://api.latoken.com/v2/marketOverview/orderbook/{}_BTC"
+# hitbtc = ""https://api.hitbtc.com/api/2/public/orderbook/{}BTC.json""
+# not working urls
+# wave = "https://marketdata.wavesplatform.com/api/v1/"
+# bittrex = "https://api.bittrex.com/api/v1.1/public/getorderbook?market=BTC&type=both"
+
+
+
+def getMarket(market, URL):
+    print("\nRetreiving data from: " + market)
+    currentMarket = {}
+    for c in CURRENCIES:
+        info = {}
+        print(c, end =" ")
+        respons = requests.get(URL.format(c))
+        info['asks'] = respons.json()['asks']
+        info['bids'] = respons.json()['bids']
+        currentMarket[c] = info
+    return currentMarket
+
+def getMarketWithTick(market, URL):
+    #Biki market needs "tick" to respond so it have to has own function
+    print("\nRetreiving data from: " + market)
+    currentMarket = {}
+    for c in CURRENCIES:
+        info = {}
+        print(c, end =" ")
+        respons = requests.get(URL.format(c.lower()))
+        info['asks'] = respons.json()['data']['tick']['asks']
+        info['bids'] = respons.json()['data']['tick']['bids']
+        currentMarket[c] = info
+    return currentMarket
+
+def calculateOffer(toMe, toMarket, asksOffers, bidsOffers, crypto):
+    if (float(asksOffers[0][1]) < float(bidsOffers[0][1])):
+        amount = float(asksOffers[0][1])
+    else:
+        amount = float(bidsOffers[0][1])
+    print("Buy {} {} in {} for {}BTC, sell in {} for {}BTC".format(
+        amount, crypto, toMe, asksOffers[0][0], toMarket, bidsOffers[0][0]))
+    global outcome
+    outcome += amount * (float(bidsOffers[0][0]) * (1 + FEE[toMarket]) - float(asksOffers[0][0]) * (1 - FEE[toMe]))
+    print("MONEEEEEY = {}BTC".format(outcome))
+
+
+def bestOfferSearch(market, orderbook):
+    for crypto, orders in orderbook.items():
+        print("Comparing {} in {} ".format(crypto, market))
+        if (orders['asks']):
+         bestAskOrder = orders['asks'][0]
+         for otherMarketName, otherOrderbook in marketsOrderbooks.items():
+            if (otherOrderbook[crypto]['bids']):
+                if (float(bestAskOrder[0]) * (1 + FEE[market]) < float(
+                        otherOrderbook[crypto]['bids'][0][0]) * (1 - FEE[otherMarketName])):
+                    calculateOffer(market, orders['asks'], otherMarketName,
+                                   otherOrderbook[crypto]['bids'], crypto)
+
+
+
+while (True):
+    marketsOrderbooks = {}
+    marketsOrderbooks['tokok'] = getMarket("Toktok", "https://www.tokok.com/api/v1/depth?symbol={}_BTC")
+    marketsOrderbooks['bitbay'] = getMarket("BitBay", "https://bitbay.net/API/Public/{}BTC/orderbook.json")
+    marketsOrderbooks['latoken'] = getMarket("Latoken", "https://api.latoken.com/v2/marketOverview/orderbook/{}_BTC")
+    marketsOrderbooks['biki'] = getMarketWithTick("Biki", "https://openapi.biki.com/open/api/market_dept?symbol={}btc&type=step0")
+
+    for market, orderbook in marketsOrderbooks.items():
+        print("\n I will now look for best offers in: " + market)
+        bestOfferSearch(market, orderbook)
+    print("Your overall outcome is astonishing {} PLN".format(outcome * getBTCtoPLN()))
