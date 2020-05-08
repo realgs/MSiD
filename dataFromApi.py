e98ab4e03e5647f3e48abab43c783c2f6e974973
TheManis92 <mateuszkogut007@gmail.com>
2020-05-08 20:33:04 +0100

    Fetch data from 4 apis

--- dataFromApi.py
+++ dataFromApi.py
@@ -18,15 +18,36 @@ def getDiffPercentage(buy_list, sell_list):
     best_sell_offer = sell_list[0][0]
     print("BUY/SELL PERCENT DIFFERENCE " + str((best_sell_offer - best_buy_offer) / (best_buy_offer) * 100))
 
-def main():
+def getBitbayData():
     url = "https://bitbay.net/API/Public/BTC/orderbook.json"
     parsed = parsedData(url)
-    buy_list = parsed["bids"]
+    buy_list = parsed["bids"][0]
+    sell_list = parsed["asks"][0]
+    return (buy_list, sell_list)
+
+def getBittrexData():
+    url = "https://api.bittrex.com/api/v1.1/public/getorderbook?market=USD-BTC&type=both"
+    parsed = parsedData(url)
+    sell_list = parsed["result"]["sell"]
+    buy_list = parsed["result"]["buy"]
+    return (buy_list, sell_list)
+
+def getBinanceData():
+    url = "https://api.binance.com/api/v3/depth?symbol=BTCTUSD"
+    parsed = parsedData(url)
+    #print(parsed)
     sell_list = parsed["asks"]
-    printOfferList("Buy","BUY OFFERS",buy_list)
-    printOfferList("Sell", "SELL OFFERS",sell_list)
-    getDiffPercentage()
+    buy_list = parsed["bids"]
+    return (buy_list, sell_list)
     
+def getBitstampData():
+    url = "https://www.bitstamp.net/api/v2/order_book/BTCUSD/""
+    parsed = parsedData(url)
+    #print(parsed)
+    sell_list = parsed["asks"]
+    buy_list = parsed["bids"]
+    return (buy_list, sell_list)
+
 def runMainEvery5Seconds():
     try:
         threading.Timer(5.0, runMainEvery5Seconds).start()
