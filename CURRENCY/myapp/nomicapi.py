eb9b596c7ede1d8c5a5e005394d285b6ddac3634
IwoZachary <61707966+IwoZachary@users.noreply.github.com>
2020-05-11 22:49:55 +0200

    Deleted unnessesary stuff

--- CURRENCY/myapp/nomicapi.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-url = "https://api.nomics.com/v1/markets?key=d89640265d015f1144316d4702afded7"
-print(urllib.request.urlopen(url).read())
--- Sort3i4/Sort3i4/Generator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Generator {
-public:
-	Generator(int max);
-	void vGenerate(int* tab,int length);
-private:
-	int max;
-};
