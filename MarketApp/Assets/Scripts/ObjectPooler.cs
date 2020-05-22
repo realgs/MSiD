45232d8342309b09af4e81a6d9122fb965fcb6c4
Kraft_ <kraftmajnmail@gmail.com>
2020-05-23 00:35:11 +0200

    base for player controls, object pooler added, tmp assets

---
+++ MarketApp/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefab;
+    private List<GameObject> objectsPool = new List<GameObject>();
+
+    private GameObject FindGameObject()
+    {
+        foreach(GameObject obj in objectsPool)
+        {
+            if (!obj.activeSelf) return obj;
+        }
+
+        return CreateNewObject();
+    }
+
+    private GameObject CreateNewObject()
+    {
+        return Instantiate(prefab, transform);
+    }
+
+    public GameObject GetGameObject()
+    {
+        return FindGameObject();
+    }
+}
---
+++ MarketApp/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private string obstacleString;
+    private new Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag(obstacleString))
+        {
+            Death();
+        }
+    }
+
+    private void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            rigidbody.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    private void Respawn()
+    {
+        transform.position = Vector2.zero;
+        rigidbody.velocity = Vector2.zero;
+    }
+
+    private void Death()
+    {
+
+    }
+}
