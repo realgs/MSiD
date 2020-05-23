c8ffb64093fbaefa751e9936dce732bc0deee78b
Dzmitry Lisouski <dmtrqerlly@gmail.com>
2020-05-23 22:28:24 +0300

    data collection and structuring

--- node/routes/home.js
+++ node/routes/home.js
@@ -3,37 +3,120 @@ var async = require('async');
 
 exports.get = function (req, res) {
     async.parallel({
-        cex: (callback) => {
-            request.get('https://cex.io/api/order_book/BTC/USD/', (err, resp, body) => {
-                if (err) console.log(err);
-
-                callback(null, JSON.parse(resp.body));
-            })
-        },
         bitstamp: (callback) => {
             request.get('https://www.bitstamp.net/api/order_book/btcusd', (err, resp, body) => {
                 if (err) console.log(err);
 
-                callback(null, JSON.parse(resp.body));
+                var r = JSON.parse(resp.body);
+
+                var bids = [];
+                var asks = [];
+
+                for (var i = 0; i < r.bids.length; i++) {
+                    bids[i] = new Array(1);
+                    bids[i][0] = r.bids[i][0];
+                    bids[i][1] = r.bids[i][1];
+                }
+
+                for (var i = 0; i < r.asks.length; i++) {
+                    asks[i] = new Array(1);
+                    asks[i][0] = r.asks[i][0];
+                    asks[i][1] = r.asks[i][1];
+                }
+
+                console.log("ok")
+
+                callback(null, {
+                    bids: bids,
+                    asks: asks
+                })
             })
         },
         bittrex: (callback) => {
             request.get('https://api.bittrex.com/api/v1.1/public/getorderbook?market=USD-BTC&type=both', (err, resp, body) => {
                 if (err) console.log(err);
 
-                callback(null, JSON.parse(resp.body));
+                var r = JSON.parse(resp.body);
+
+                var bids = [];
+                var asks = [];
+
+                for (var i = 0; i < r.result.buy.length; i++) {
+                    bids[i] = new Array(1);
+                    bids[i][0] = r.result.buy[i].Rate;
+                    bids[i][1] = r.result.buy[i].Quantity;
+                }
+
+                for (var i = 0; i < r.result.sell.length; i++) {
+                    asks[i] = new Array(1);
+                    asks[i][0] = r.result.sell[i].Rate;
+                    asks[i][1] = r.result.sell[i].Quantity;
+                }
+
+                console.log("ok")
+
+                callback(null, {
+                    bids: bids,
+                    asks: asks
+                })
+            })
+        },
+        cex: (callback) => {
+            request.get('https://cex.io/api/order_book/BTC/USD/', (err, resp, body) => {
+                if (err) console.log(err);
+
+                var r = JSON.parse(resp.body);
+
+                var bids = [];
+                var asks = [];
+
+                for (var i = 0; i < r.bids.length; i++) {
+                    bids[i] = new Array(1);
+                    bids[i][0] = r.bids[i][0];
+                    bids[i][1] = r.bids[i][1];
+                }
+
+                for (var i = 0; i < r.asks.length; i++) {
+                    asks[i] = new Array(1);
+                    asks[i][0] = r.asks[i][0];
+                    asks[i][1] = r.asks[i][1];
+                }
+
+                callback(null, {
+                    bids: bids,
+                    asks: asks
+                })
             })
         },
         bitbay: (callback) => {
             request.get('https://bitbay.net/API/Public/BTC/orderbook.json', (err, resp, body) => {
                 if (err) console.log(err);
 
-                callback(null, JSON.parse(resp.body));
+                var r = JSON.parse(resp.body);
+
+                var asks = [];
+                var bids = [];
+
+                for (var i = 0; i < r.bids.length; i++) {
+                    bids[i] = new Array(1);
+                    bids[i][0] = r.bids[i][0];
+                    bids[i][1] = r.bids[i][1];
+                }
+
+                for (var i = 0; i < r.asks.length; i++) {
+                    asks[i] = new Array(1);
+                    asks[i][0] = r.asks[i][0];
+                    asks[i][1] = r.asks[i][1];
+                }
+
+                callback(null, {
+                    bids: bids,
+                    asks: asks
+                })
             })
         }
     }, (err, result) => {
-        console.log(typeof result);
 
-        res.send(result);
+        res.send(result)
     })
 }
--- node/routes/index.js
+++ node/routes/index.js
@@ -2,4 +2,5 @@ var request = require('request');
 
 module.exports = (app) => {
     app.get('/', require('./home').get);
+    app.get('/test', require('./test').get);
 }
