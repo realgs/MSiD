a08e67df84cb8e36d204b0dd1d0dd57c28c8674a
Kraft_ <kraftmajnmail@gmail.com>
2020-06-05 18:12:05 +0200

    init L6

---
+++ simulator.py
@@ -0,0 +1,10 @@
+#fetch data
+#parse data
+#implement knn
+#knn to detect fall or rise - look for similiar opens/closes/volumes
+#knn to detect how much - look for similiar probabilities
+#plot results
+
+#knn references: 
+#https://machinelearningmastery.com/tutorial-to-implement-k-nearest-neighbors-in-python-from-scratch/
+#https://github.com/sammanthp007/Stock-Price-Prediction-Using-KNN-Algorithm
