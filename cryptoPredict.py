38d1a7a446aecb4c95b9fcdef31b32c58684913e
TheManis92 <mateuszkogut007@gmail.com>
2020-06-06 19:41:16 +0100

    Add multiple simulation

--- cryptoPredict.py
+++ cryptoPredict.py
@@ -16,16 +16,17 @@ def growthOrLoss(probability):
 
 def simulate(iterations, x, reg):
     sim_result = []
-    for i in range(0, iterations-1):
+    for i in range(0, iterations):
         sim_day = []
         y = reg.predict(x)
         y = y.tolist()
         x = x.tolist()
         x = x[0]
+        sim_day.append(1)
         sim_day.append(abs(y[0][0]))
         sim_day.append(abs(y[0][1]))
-        sim_day.append(growthOrLoss(y[0][2])*abs(y[0][0])*x[2]+x[2])
-        sim_day.append(growthOrLoss(y[0][3])*abs(y[0][1])*x[3]+x[3])
+        sim_day.append(growthOrLoss(y[0][2])*abs(y[0][0])*x[3]+x[3])
+        sim_day.append(growthOrLoss(y[0][3])*abs(y[0][1])*x[4]+x[4])
         sim_day.append(sim_day[2]/sim_day[3])
         x = sim_day
         x = np.matrix(x)
@@ -46,16 +47,17 @@ def start(dateFrom,dateTo):
         if record[0] > dt:
             break
         data_day = []
+        data_day.append(1)
         data_day.append(abs((float(record[4]) - float(record[1]))/float(record[1]))) 
         if i > 0:
-            data_day.append(abs((dataset[i-1][5] - float(record[6]))/dataset[i-1][5]))
+            data_day.append(abs((dataset[i-1][6] - float(record[6]))/dataset[i-1][6]))
         else:
             data_day.append(0)
         if i > 0 and ((float(record[4]) - float(record[1]))/float(record[1])) > 0:
             data_day.append(1)
         else:
             data_day.append(0)
-        if i > 0 and ((dataset[i-1][5] - float(record[6]))/dataset[i-1][5]) > 0:
+        if i > 0 and ((dataset[i-1][6] - float(record[6]))/dataset[i-1][6]) > 0:
             data_day.append(1)
         else:
             data_day.append(0)
@@ -67,19 +69,21 @@ def start(dateFrom,dateTo):
 
     D = np.matrix(dataset)
     D = np.squeeze(np.asarray(D))
-    y = D[1:,[0,1,2,3]]
-    X = D[:-1,[0,1,4,5,6]]
-    print(X)
-    print(y)
+    y = D[1:,[1,2,3,4]]
+    X = D[:-1,[0,1,2,5,6,7]]
     regressor = LinearRegression()
     model = regressor.fit(X, y)
 
-    X_pred = D[-1:,[0,1,4,5,6]]
-    print(np.matrix(simulate(10, X_pred, model)))
-    
+    X_pred = D[-1:,[0,1,2,5,6,7]]
+    single_simulation = np.matrix(simulate(int((dt-df)/86400), X_pred, model))
+    multi_simulation = []
+    for i in range (0,100):
+        multi_simulation.append(np.matrix(simulate(int((dt-df)/86400), X_pred, model)))
+        print(i)
+    print("Done")
 
 if __name__ == "__main__":
-    start("01/04/2020","01/06/2020")
+    start("01/02/2020","01/06/2020")
     #print(random.uniform(0,1))
 
 
