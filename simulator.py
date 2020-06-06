55956d6825033cbe7541468d13fec7db0d43a7b2
Sebastian <spieta2@gmail.com>
2020-06-06 23:23:12 +0200

    parser added

--- simulator.py
+++ simulator.py
@@ -1,5 +1,5 @@
 #fetch data - DONE
-#parse data
+#parse data - DONE
 #implement knn
 #knn to detect fall or rise - look for similiar opens/closes/volumes
 #knn to detect how much - look for similiar probabilities
@@ -13,6 +13,19 @@ import requests as req
 
 def fetch_data(currency, startTime, endTime, step, limit):
     data = req.get("https://www.bitstamp.net/api/v2/ohlc/{0}/".format(currency), params={'start': startTime, 'end': endTime, 'step': step, 'limit': limit})
-    return data.json()
+    return data.json()['data']['ohlc']
 
-print(fetch_data("btcusd", 1591040415, 1591440415, 259200, 1000))
+def parse_data(rawData):
+    output = []
+    for d in rawData:
+        entry = {}
+        entry['timestamp'] = d['timestamp']
+        entry['diff'] = float(d['close'])-float(d['open'])
+        entry['volume'] = d['volume']
+        output.append(entry)
+    return output
+
+data = fetch_data("btcusd", 1591040415, 1591440415, 86400, 50)
+parsed = parse_data(data)
+for p in parsed:
+    print(p)
