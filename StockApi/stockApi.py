e2205f9d6a6833ed64a5ebfd28a7ba1e433cc9b1
Elunniva <elunniva@wp.pl>
2020-06-06 21:03:29 +0200

    added calculating arbitrage, values,  printing data from and saving in db

--- StockApi/stockApi.py
+++ StockApi/stockApi.py
@@ -4,11 +4,12 @@ import tkinter
 from tkinter import simpledialog
 from tkinter import messagebox
 import sqlite3
+from forex_python.bitcoin import BtcConverter
 
 conn = sqlite3.connect('database.db', check_same_thread=False)
 c = conn.cursor()
 window = tkinter.Tk()
-
+cur = BtcConverter()
 
 markets = [
     ('BTC', 'USD'),
@@ -38,15 +39,14 @@ urls = {
     'bitfinex': 'https://api-pub.bitfinex.com/v2/book/t{0}{1}/P0?len=1',
     'bitstamp': 'https://www.bitstamp.net/api/v2/order_book/{0}{1}',
 }
-"""
-budget = {
-    'USD': 1000.0,
-    'BTC': 0.1,
-    'LTC': 20.0,
-    'XRP': 4500.0,
-    'ETH': 5.0
+
+availableCurr = {
+    'USD',
+    'BTC',
+    'LTC',
+    'XRP',
+    'ETH',
 }
-"""
 
 
 def get_buy_sell_list(api, market):
@@ -84,21 +84,29 @@ def find_best_sell(sell):
 
     return min_b
 
-"""
+
 def get_best_arbitrage(buy, sell, market):
+    wallet = get_wallet()
     buy = find_best_buy(buy)
     sell = find_best_sell(sell)
     quantity = min(buy[1], sell[1])
-    if quantity * sell[0] > budget[market[1]]:
-        quantity = budget[market[1]] / sell[0]
-    profit = quantity * (buy[0] - sell[0] - buy[0] * taker[buy[2]] - sell[0] * taker[sell[2]])
-    if profit > 0:
-        print(f'You can buy {quantity} of {market[0]} in {market[1]} on {sell[2]} for {sell[0]} '
-              f'and sell on {buy[2]} for {buy[0]} '
-              f'gaining {profit} {market[1]}')
-        budget[market[1]] += profit
-"""
-"""
+    budget = [m for m in wallet if m[1] == market[1]]
+    if budget:
+        budget_value = budget[0][2]
+        if quantity * sell[0] > budget_value:
+            quantity = budget_value / sell[0]
+        profit = quantity * (buy[0] - sell[0] - buy[0] * taker[buy[2]] - sell[0] * taker[sell[2]])
+        if profit > 0:
+            messagebox.showinfo('Transaction info', f'You can buy {quantity} of {market[0]} in {market[1]} on {sell[2]} for {sell[0]} '
+                f'and sell on {buy[2]} for {buy[0]} '
+                f'gaining {profit} {market[1]}')
+            c.execute("INSERT INTO Transactions (Quantity, MarketC, MarketC2, ApiS, AmountS, ApiB, AmountB, Profit)"
+                      " VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                      (quantity, market[0], market[1], sell[2], sell[0], buy[2], buy[0], profit))
+            c.execute("UPDATE Currencies SET Amount = ? WHERE Name = ?", (budget_value+profit, market[1]))
+            conn.commit()
+
+
 def calculate_best_arbitrage():
     for market in markets:
         buy = []
@@ -108,14 +116,13 @@ def calculate_best_arbitrage():
             buy.append(data[0]+(api,))
             sell.append(data[1]+(api,))
         get_best_arbitrage(buy, sell, market)
-    print(budget)
-"""
-"""
+
+
+
 def update_best_arbitrage():
     while True:
         calculate_best_arbitrage()
         sleep(5)
-"""
 
 
 # list 3
@@ -150,6 +157,10 @@ def get_wallet():
     return fetchall("SELECT * FROM Currencies")
 
 
+def get_transactions():
+    return fetchall("SELECT * FROM Transactions")
+
+
 def get_base_currency():
     return fetchall("SELECT * FROM Base")
 
@@ -157,6 +168,10 @@ def get_base_currency():
 def change_base_currency(labelText):
     base_currency = simpledialog.askstring("Input", "In which base currency you want your wallet?", parent=window)
     if not base_currency:
+        return
+    if base_currency not in availableCurr:
+        messagebox.showerror('Error', f'No such currency, choose from {availableCurr}')
+        change_base_currency()
         return
     if get_base_currency():
         c.execute("UPDATE Base SET Name = ? WHERE IdB = ?", (base_currency, 1))
@@ -178,6 +193,10 @@ def create_new_wallet(labelText):
 def add_currency():
     currency = simpledialog.askstring("Input", "What currency you want to add?", parent=window)
     if not currency:
+        return
+    if currency not in availableCurr:
+        messagebox.showerror('Error', f'No such currency, choose from {availableCurr}')
+        add_currency()
         return
     currency_amount = simpledialog.askinteger("Input", "How much you want to add?", parent=window)
     if not currency_amount:
@@ -206,11 +225,57 @@ def update_currency():
 
 
 def calculate_values():
-    return
+    c.execute("SELECT * FROM Transactions ORDER BY IdT DESC LIMIT 1")
+    result = c.fetchone()
+    result = list(result)
+    if result:
+        base = get_base_currency()
+        base = base[0][1]
+        if base == 'USD':
+            if result[3] == 'BTC':
+                result[7] = cur.convert_btc_to_cur(result[7], 'USD')
+                result[8] = cur.convert_btc_to_cur(result[8], 'USD')
+            if result[2] == 'BTC':
+                result[5] = cur.convert_btc_to_cur(result[5], 'USD')
+        elif base == 'BTC':
+            if result[3] == 'USD':
+                result[7] = cur.convert_to_btc(result[7], 'USD')
+                result[8] = cur.convert_to_btc(result[8], 'USD')
+            if result[2] == 'USD':
+                result[5] = cur.convert_to_btc(result[5], 'USD')
+        else:
+            messagebox.showinfo('Info', 'Only converting currency USD BTC, values shown without converting')
+        messagebox.showinfo(f'Last transaction values in {base}', f'You can buy {result[1]} of {result[2]} in {result[3]} on {result[4]} for {result[5]} '
+                    f'and sell on {result[6]} for {result[7]} '
+                    f'gaining {result[8]} {base}')
+    else:
+        messagebox.showerror('Error', 'No transactions available')
+
+
+def print_wallet():
+    print_window = tkinter.Tk()
+    print_window.title('Wallet')
+    data = get_wallet()
+    data_labels = [[tkinter.Label(print_window, text=str(y)) for y in x] for x in data]
+    for i, labels in enumerate(data_labels):
+        for j, label in enumerate(labels):
+            label.grid(row=i, column=j, padx=5, pady=1)
+
+def print_transactions():
+    data = get_transactions()
+    if data:
+        print_window = tkinter.Tk()
+        print_window.title('Wallet')
+        data_labels = [[tkinter.Label(print_window, text=str(y)) for y in x] for x in data]
+        for i, labels in enumerate(data_labels):
+            for j, label in enumerate(labels):
+                label.grid(row=i, column=j, padx=5, pady=1)
+    else:
+        messagebox.showerror('Error', 'List of transactions empty')
 
 
 def create_main_window():
-    window.title('Wallet')
+    window.title('Manage your wallet')
     labelText = tkinter.StringVar()
     base_currency = get_base_currency()
 
@@ -221,24 +286,25 @@ def create_main_window():
     label = tkinter.Label(window, textvariable=labelText)
     buttons_data = [
         ('Change base currency', lambda: change_base_currency(labelText)),
+        ('Show your wallet', print_wallet),
+        ('Calculate best arbitrage', calculate_best_arbitrage()),
         ('New wallet', lambda: create_new_wallet(labelText)),
         ('Add currency', add_currency),
         ('Delete currency', delete_currency),
         ('Change currency amount', update_currency),
-        ('Calculate values', calculate_values),
+        ('Show all transactions', print_transactions),
+        ('Calculate values of last transaction', calculate_values),
         ('Quit', window.quit),
     ]
 
     label.grid(pady=10)
-    buttons = [tkinter.Button(window, text=b[0], command=b[1])
-               for b in buttons_data]
+    buttons = [tkinter.Button(window, text=b[0], command=b[1]) for b in buttons_data]
 
     for button in buttons:
         button.grid(sticky='nesw', padx=5)
 
 
 if __name__ == "__main__":
-    # list 4 update_best_arbitrage()
     create_main_window()
     window.mainloop()
 
