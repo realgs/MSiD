1aac387106f785b0a99d338aff2d443aa7eb0302
Sebastian <spieta2@gmail.com>
2020-06-07 11:40:57 +0200

    plotter base

--- simulator.py
+++ simulator.py
@@ -10,6 +10,7 @@
 #https://github.com/sammanthp007/Stock-Price-Prediction-Using-KNN-Algorithm
 
 import requests as req
+import matplotlib.pyplot as plot
 
 def fetch_data(currency, startTime, endTime, step, limit):
     data = req.get("https://www.bitstamp.net/api/v2/ohlc/{0}/".format(currency), params={'start': startTime, 'end': endTime, 'step': step, 'limit': limit})
@@ -25,7 +26,17 @@ def parse_data(rawData):
         output.append(entry)
     return output
 
+def plotter(data):
+    timestamps = []
+    difs = []
+    for d in data:
+        timestamps.append(d['timestamp'])
+        difs.append(d['diff'])
+
+    plot.plot(timestamps, difs)
+    plot.show()
 data = fetch_data("btcusd", 1591040415, 1591440415, 86400, 50)
 parsed = parse_data(data)
 for p in parsed:
     print(p)
+plotter(parsed)
