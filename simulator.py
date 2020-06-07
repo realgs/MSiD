f005faf50e2364c57f99713f4027553b6b9ea737
Sebastian <spieta2@gmail.com>
2020-06-07 12:09:38 +0200

    updated parser, added distance func

--- simulator.py
+++ simulator.py
@@ -14,17 +14,51 @@ import matplotlib.pyplot as plot
 
 def fetch_data(currency, startTime, endTime, step, limit):
     data = req.get("https://www.bitstamp.net/api/v2/ohlc/{0}/".format(currency), params={'start': startTime, 'end': endTime, 'step': step, 'limit': limit})
-    return data.json()['data']['ohlc']
-
+    return data.json()
+#   RAW DATA
+#pair	    Trading pair
+#high	    Price high
+#timestamp	Unix timestamp date and time
+#volume	    Volume
+#low	    Price low
+#close  	Closing price
+#open       Opening price
 def parse_data(rawData):
+    rawData = rawData['data']['ohlc']
     output = []
     for d in rawData:
         entry = {}
-        entry['timestamp'] = d['timestamp']
-        entry['diff'] = float(d['close'])-float(d['open'])
-        entry['volume'] = d['volume']
+        entry['timestamp'] = int(d['timestamp'])
+        entry['close'] = float(d['close'])
+        entry['open'] = float(d['open'])
+        entry['volume'] = float(d['volume'])
+        entry['low'] = float(d['low'])
+        entry['high'] = float(d['high'])
         output.append(entry)
     return output
+
+def nearest_neighbours(data):
+    pass
+
+def distance(dataRow1, dataRow2):
+    dist = 0.0
+    dist+=(dataRow1['timestamp'] - dataRow2['timestamp'])**2
+    dist+=(dataRow1['close'] - dataRow2['close'])**2
+    dist+=(dataRow1['open'] - dataRow2['open'])**2
+    dist+=(dataRow1['volume'] - dataRow2['volume'])**2
+    dist+=(dataRow1['low'] - dataRow2['low'])**2
+    dist+=(dataRow1['high'] - dataRow2['high'])**2
+    return dist**(1/2)
+
+
+def predict_direction(data):
+    pass
+
+def predict_value(data):
+    pass
+
+def simulate(data):
+    pass
 
 def plotter(data):
     timestamps = []
@@ -35,6 +69,7 @@ def plotter(data):
 
     plot.plot(timestamps, difs)
     plot.show()
+
 data = fetch_data("btcusd", 1591040415, 1591440415, 86400, 50)
 parsed = parse_data(data)
 for p in parsed:
